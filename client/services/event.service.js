168112f190e0bca74da90821a938d436e84e486d
Jesse Buitenhuis <jessebuitenhuis@gmail.com>
2015-11-06 23:44:22 +0100

    date fix applied again

--- client/services/event.service.js
+++ client/services/event.service.js
@@ -1,5 +1,10 @@
 angular.module('gigPlanner').factory('Event', function($resource){
    return $resource('/api/events/:id', {id: '@_id'}, {
+       get: {method: 'GET', transformResponse: function(data){
+           data = angular.fromJson(data);
+           if (data.date) data.date = new Date(data.date);
+           return data;
+       }},
        update: { method: 'PUT' },
        addUser: { method: 'POST', url: '/api/events/:id/users/:userId' },
        removeUser: { method: 'DELETE', url: '/api/events/:id/users/:docId' }
